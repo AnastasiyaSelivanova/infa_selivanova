d82a7afb84ace663241bf89312b9dfb06e0f9b9d
AnastasiyaSelivanova <148740374+AnastasiyaSelivanova@users.noreply.github.com>
2023-10-29 19:23:16 +0700

    Update car.py

--- car.py
+++ car.py
@@ -1,30 +1,26 @@
-import yolov5
+from transformers import DetrImageProcessor, DetrForObjectDetection
+import torch
+from PIL import Image
+import requests
 
-# load model
-model = yolov5.load('keremberke/yolov5m-license-plate')
-  
-# set model parameters
-model.conf = 0.25  # NMS confidence threshold
-model.iou = 0.45  # NMS IoU threshold
-model.agnostic = False  # NMS class-agnostic
-model.multi_label = False  # NMS multiple labels per box
-model.max_det = 1000  # maximum number of detections per image
+url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
 
-# set image
-img = 'https://github.com/ultralytics/yolov5/raw/master/data/images/zidane.jpg'
+# you can specify the revision tag if you don't want the timm dependency
+processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
 
-# perform inference
-results = model(img, size=640)
+inputs = processor(images=image, return_tensors="pt")
+outputs = model(**inputs)
 
-# inference with test time augmentation
-results = model(img, augment=True)
+# convert outputs (bounding boxes and class logits) to COCO API
+# let's only keep detections with score > 0.9
+target_sizes = torch.tensor([image.size[::-1]])
+results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
 
-# parse results
-predictions = results.pred[0]
-boxes = predictions[:, :4] # x1, y1, x2, y2
-scores = predictions[:, 4]
-categories = predictions[:, 5]
-
-# show detection bounding boxes on image
-results.show()
-
+for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+    box = [round(i, 2) for i in box.tolist()]
+    print(
+            f"Detected {model.config.id2label[label.item()]} with confidence "
+            f"{round(score.item(), 3)} at location {box}"
+    )
